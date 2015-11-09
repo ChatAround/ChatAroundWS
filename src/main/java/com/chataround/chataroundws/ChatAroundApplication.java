6f3dd2d784e8ceec28cb6f5fc3818ddce5dfddb3
Gewrgia <georgiagrigor@gmail.com>
2015-11-09 11:42:51 +0200

    Initial commit

---
+++ src/main/java/com/chataround/chataroundws/ChatAroundApplication.java
@@ -0,0 +1,12 @@
+package com.chataround.chataroundws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAroundApplication.class, args);
+    }
+}
