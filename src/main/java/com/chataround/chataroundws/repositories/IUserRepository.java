0b75b66262414299b5ef8921eb9b5b0b089f659c
Gewrgia <georgiagrigor@gmail.com>
2015-11-10 19:26:51 +0200

    created IUserService and UserService for returning all Users

--- src/main/java/com/chataround/chataroundws/repositories/IUserRepository.java
+++ src/main/java/com/chataround/chataroundws/repositories/IUserRepository.java
@@ -2,7 +2,11 @@ package com.chataround.chataroundws.repositories;
 
 import com.chataround.chataroundws.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 /**
  * Created by Gewrgia on 10/11/2015.
--- src/main/java/com/chataround/chataroundws/services/IUserService.java
+++ src/main/java/com/chataround/chataroundws/services/IUserService.java
@@ -2,8 +2,6 @@ package com.chataround.chataroundws.services;
 
 import com.chataround.chataroundws.models.DTOs.UserDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Collection;
 
@@ -11,6 +9,6 @@ import java.util.Collection;
  * Created by Gewrgia on 10/11/2015.
  */
 public interface IUserService {
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    ResponseEntity<Collection<UserDTO>> getAllUsers();
+
+    public ResponseEntity<Collection<UserDTO>> getAllUsers();
 }
